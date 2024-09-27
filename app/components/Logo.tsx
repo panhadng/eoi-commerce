d93867f8d8a97c73f33b3b34ead7b56d3b0e7461
Ratanak Panha Duong <ratanakpanhaduong@Ratanaks-MacBook-Pro.local>
2024-09-27 16:09:48 +1000

    add new logo

---
+++ app/components/Logo.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FaDollarSign } from "react-icons/fa";
+
+const Logo = () => {
+  return (
+    <div className="bg-gray-900 rounded-full h-full w-auto p-2">
+      <FaDollarSign className="text-xl text-white" />
+    </div>
+  );
+};
+
+export default Logo;
--- app/components/Topbar.tsx
+++ app/components/Topbar.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { FaBell, FaComment, FaUser } from "react-icons/fa";
+import { FaBell, FaCommentAlt } from "react-icons/fa";
+import Logo from "@/app/components/Logo";
 
 const Topbar: React.FC = () => {
   return (
-    <div className="flex items-center justify-between p-5 w-full border-b border-gray-300">
-      <FaUser className="text-xl" />
+    <div className="flex items-center justify-between p-3 w-full border-b border-gray-300">
+      <Logo />
       <p>Topbar</p>
       <div className="flex space-x-4">
-        <FaComment className="text-xl" />
+        <FaCommentAlt className="text-xl" />
         <FaBell className="text-xl" />
       </div>
     </div>
